8b718ae89052358e6cfad405235f8534e030da4d
Alberto Scala <alberto.scala.it@gmail.com>
2026-08-24 11:16:44 +0200

    refactored functions

--- include/qemu_dev.h
+++ include/qemu_dev.h
@@ -7,19 +7,18 @@
 #define UART_LSR    (*(volatile unsigned char *)(UART_BASE + 0x05))
 #define LSR_THRE    (1 << 5)
 
-#define UART0(S)    puts(S)
+#define UART0(S)    UART_SendString(S)
 
-force_inline void putchar(char c)
+force_inline void UART_SendByte(char c)
 {
     while (!(UART_LSR & LSR_THRE)) { /* spin */ }
     UART_THR = c;
 }
 
-force_inline void puts(const char* s)
+force_inline void UART_SendString(const char* s)
 {
     for (int i = 0; s[i] != '\0'; i++)
     {
-        putchar(s[i]);
+        UART_SendByte(s[i]);
     }
-    putchar('\n');
 }
